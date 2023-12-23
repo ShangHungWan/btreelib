ba9d08d2dd4e6b9190e1c52f20edebd02875580d
Sun <a24230928@gmail.com>
2023-12-24 01:07:21 +0800

    test: exist

--- test/test_btree.py
+++ test/test_btree.py
@@ -4,7 +4,17 @@ import btree
 
 class BTreeTest(unittest.TestCase):
     def test_insert(self):
+        keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 5.5, -3.3, 0]
+        tree = btree.BTree()
+        for key in keys:
+            tree.insert(key)
+
+    def test_exist(self):
         keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         tree = btree.BTree()
         for key in keys:
             tree.insert(key)
+        for key in keys:
+            self.assertTrue(tree.exist(key))
+
+        self.assertFalse(tree.exist(0))
