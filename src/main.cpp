2ae3e5608bbe01560fd7d64e0d26418d5a177e0b
Sun <a24230928@gmail.com>
2023-12-24 19:30:10 +0800

    feat: remove

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ public:
 
     void insert_non_full(const T &key)
     {
-        size_t index = find_key(key);
+        size_t index = find_key_back(key);
 
         if (is_leaf)
         {
@@ -63,14 +63,184 @@ public:
         }
     }
 
-    size_t find_key(const T &key)
+    long long find_key_back(const T &key) const
     {
-        int index = keys.size() - 1;
+        long long index = keys.size() - 1;
         while (index >= 0 && keys[index] > key)
         {
             --index;
         }
         return index;
+    }
+
+    long long find_key_front(const T &key) const
+    {
+        long long index = 0;
+        long long size = keys.size();
+        while (index < size && key > keys[index])
+        {
+            ++index;
+        }
+        return index;
+    }
+
+    void remove(const T &key)
+    {
+        long long index = find_key_front(key);
+        long long size = keys.size();
+
+        if (index < size && keys[index] == key)
+        {
+            if (is_leaf)
+            {
+                keys.erase(keys.begin() + index);
+            }
+            else
+            {
+                if (children[index]->keys.size() >= ORDER)
+                {
+                    T pred = get_predecessor(index);
+                    children[index]->remove(pred);
+                    keys[index] = pred;
+                }
+                else if (children[index + 1]->keys.size() >= ORDER)
+                {
+                    T succ = get_successor(index);
+                    children[index + 1]->remove(succ);
+                    keys[index] = succ;
+                }
+                else
+                {
+                    merge_children(index);
+                    children[index]->remove(key);
+                }
+            }
+        }
+        else
+        {
+            bool last_child = (index == size);
+
+            if (children[index]->keys.size() < ORDER)
+            {
+                if (index > 0 && children[index - 1]->keys.size() >= ORDER)
+                {
+                    borrow_from_prev(index);
+                }
+                else if (index < size && children[index + 1]->keys.size() >= ORDER)
+                {
+                    borrow_from_next(index);
+                }
+                else
+                {
+                    if (index < size)
+                    {
+                        merge_children(index);
+                    }
+                    else
+                    {
+                        merge_children(index - 1);
+                    }
+                }
+            }
+
+            if (last_child && index > size)
+            {
+                children[index - 1]->remove(key);
+            }
+            else
+            {
+                children[index]->remove(key);
+            }
+        }
+    }
+
+private:
+    T get_predecessor(int index)
+    {
+        BTreeNode<T> *current = children[index];
+        while (!current->is_leaf)
+        {
+            current = current->children.back();
+        }
+        return current->keys.back();
+    }
+
+    T get_successor(int index)
+    {
+        BTreeNode<T> *current = children[index + 1];
+        while (!current->is_leaf)
+        {
+            current = current->children.front();
+        }
+        return current->keys.front();
+    }
+
+    void borrow_from_prev(int index)
+    {
+        BTreeNode<T> *child = children[index];
+        BTreeNode<T> *sibling = children[index - 1];
+
+        for (int i = child->keys.size(); i > 0; --i)
+        {
+            child->keys[i] = child->keys[i - 1];
+        }
+        child->keys[0] = keys[index - 1];
+
+        if (!child->is_leaf)
+        {
+            for (int i = child->children.size(); i > 0; --i)
+            {
+                child->children[i] = child->children[i - 1];
+            }
+            child->children[0] = sibling->children.back();
+            sibling->children.pop_back();
+        }
+
+        keys[index - 1] = sibling->keys.back();
+        sibling->keys.pop_back();
+    }
+
+    void borrow_from_next(int index)
+    {
+        BTreeNode<T> *child = children[index];
+        BTreeNode<T> *sibling = children[index + 1];
+
+        child->keys.push_back(keys[index]);
+
+        if (!child->is_leaf)
+        {
+            child->children.push_back(sibling->children.front());
+            sibling->children.erase(sibling->children.begin());
+        }
+
+        keys[index] = sibling->keys.front();
+        sibling->keys.erase(sibling->keys.begin());
+    }
+
+    void merge_children(int index)
+    {
+        BTreeNode<T> *child = children[index];
+        BTreeNode<T> *sibling = children[index + 1];
+
+        child->keys.push_back(keys[index]);
+
+        for (const T &key : sibling->keys)
+        {
+            child->keys.push_back(key);
+        }
+
+        if (!child->is_leaf)
+        {
+            for (BTreeNode<T> *c : sibling->children)
+            {
+                child->children.push_back(c);
+            }
+        }
+
+        keys.erase(keys.begin() + index);
+        children.erase(children.begin() + index + 1);
+
+        delete sibling;
     }
 };
 
@@ -99,7 +269,7 @@ public:
         BTreeNode<T> *node = root;
         while (node != nullptr)
         {
-            size_t index = node->find_key(key);
+            size_t index = node->find_key_back(key);
             if (index < node->keys.size() && node->keys[index] == key)
             {
                 return true;
@@ -114,6 +284,17 @@ public:
             }
         }
         return false;
+    }
+
+    void remove(const T &key)
+    {
+        root->remove(key);
+        if (root->keys.empty() && !root->is_leaf)
+        {
+            BTreeNode<T> *old_root = root;
+            root = root->children[0];
+            delete old_root;
+        }
     }
 
 private:
@@ -133,5 +314,6 @@ PYBIND11_MODULE(btree, m)
     pybind11::class_<BTree<double>>(m, "BTree")
         .def(pybind11::init<>())
         .def("insert", &BTree<double>::insert)
-        .def("exist", &BTree<double>::exist);
+        .def("exist", &BTree<double>::exist)
+        .def("remove", &BTree<double>::remove);
 }
